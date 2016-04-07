bcf2415ef2bfb57c091878249ae2a73e3ceceb94
Joe Lagasse <joe@servant.co>
2016-04-07 00:57:49 -0400

    Textarea functions properly and has tests

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,16 +3,37 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
   let component;
-  
+
   beforeEach(() => {
     component = renderComponent(CommentBox);
   });
   
+  // it('has the correct class', () => {
+  //   expect(component.find(''))
+  // });
+
   it('has a text area', () => {
     expect(component.find('textarea')).to.exist;
   });
 
-  it('it has a button', () => {
+  it('has a button', () => {
     expect(component.find('input')).to.exist;
   });
+
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+    
+    it('shows that text in the textarea', () => {
+      expect(component.find('textarea')).to.have.value('new comment');
+    });
+    
+    it('when submitted, clears the input', () => {
+      component.simulate('submit');
+      expect(component.find('textarea')).to.have.value('');
+    });
+
+  });
+
 });
